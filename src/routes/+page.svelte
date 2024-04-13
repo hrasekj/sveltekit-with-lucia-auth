a15a644efbe41aefce759199ef7e7f906bd8254a
Jakub Hrášek <hrasekj@gmail.com>
2024-04-13 21:22:46 +0200

    feat: update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { LayoutData } from './$types';
+  export let data: LayoutData;
+</script>
+
+<h1>Welcome to Lucia Auth Example</h1>
+<p>This is a simple example of how to use Lucia with Auth.</p>
+
+{#if data.user}
+<p>Welcome, {data.user.email}!</p>
+{:else}
+<p>You are not logged in.</p>
+{/if}
