68fe5af9e5ca14b58d856346a92909b874dece97
Jakub Hrášek <hrasekj@gmail.com>
2024-04-13 21:22:39 +0200

    feat: add layout with nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { LayoutData } from './$types';
+  export let data: LayoutData;
+</script>
+
+<svelte:head>
+  <title>Lucia Auth Example</title>
+</svelte:head>
+
+<main>
+  <nav>
+    <a href="/">Home</a>
+
+    {#if data.user}
+    <a href="/profile">Profile</a>
+    <a href="/logout">Logout</a>
+    {:else}
+    <a href="/login">Login</a>
+    <a href="/signup">Signup</a>
+    {/if}
+  </nav>
+
+  <slot />
+</main>
