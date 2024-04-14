7d1eba8ac80b031c4ce7f8f87e3725acaa3be163
Jakub Hrášek <hrasekj@gmail.com>
2024-04-14 11:42:45 +0200

    feat: stringify uuid from binary

--- src/lib/server/database/columns/uuid.ts
+++ src/lib/server/database/columns/uuid.ts
@@ -1,14 +1,18 @@
 import { sql } from 'drizzle-orm';
 import type { MySqlColumn } from 'drizzle-orm/mysql-core';
 import { customType } from 'drizzle-orm/mysql-core';
+import { stringify as uuidStringify } from 'uuid';
 
 // @see https://dev.mysql.com/blog-archive/mysql-8-0-uuid-support/
 
 export type UUID = string;
 
-export const uuidColumn = customType<{ data: UUID }>({
+export const uuidColumn = customType<{ data: UUID; driverData: Buffer }>({
   dataType: () => 'binary(16)',
-  toDriver: (value) => sql<UUID>`UUID_TO_BIN(${value})`
+  toDriver: (value) => sql<UUID>`UUID_TO_BIN(${value})`,
+  // I hope this workaround will not be needed in the future.
+  // @see https://github.com/drizzle-team/drizzle-orm/issues/2159
+  fromDriver: (value) => uuidStringify(value)
 });
 
 export const uuid = (column: MySqlColumn) => sql<UUID>`BIN_TO_UUID(${column})`;
