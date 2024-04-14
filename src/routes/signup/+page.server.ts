981c1e237920b3b0306a731b5d0efef8b041e527
Jakub Hrášek <hrasekj@gmail.com>
2024-04-14 10:08:49 +0200

    feat: add signup page

---
+++ src/routes/signup/+page.server.ts
@@ -0,0 +1,49 @@
+import { createSessionCookie, lucia } from '$lib/server/auth';
+import { userRepository } from '$lib/server/db';
+import { generateRandomId, hashPassword } from '$lib/server/password';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+  default: async (event) => {
+    const formData = await event.request.formData();
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (typeof email !== 'string') {
+      return fail(400, {
+        message: 'Invalid email'
+      });
+    }
+
+    if (typeof password !== 'string' || password.length < 6 || password.length > 255) {
+      return fail(400, {
+        message: 'Invalid password'
+      });
+    }
+
+    try {
+      const salt = generateRandomId();
+      const hashedPassword = await hashPassword(salt, password);
+
+      const user = await userRepository.createUser({
+        email,
+        salt,
+        password: hashedPassword
+      });
+
+      const session = await lucia.createSession(user.id, user);
+      createSessionCookie(event, session);
+    } catch (err: any) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        return fail(409, {
+          message: 'Email already in use'
+        });
+      }
+
+      throw err;
+    }
+
+    redirect(302, '/');
+  }
+};
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+  import type { ActionData } from './$types';
+
+  export let action: ActionData;
+</script>
+
+<h1>Sign in</h1>
+
+{#if action?.message}
+<p>{action.message}</p>
+{/if}
+
+<form method="post" use:enhance>
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <br />
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <br />
+  <button type="submit">Sign up</button>
+</form>
