4004c91857bc39c6de6333e7423aa566a026f95d
Jakub Hrášek <hrasekj@gmail.com>
2024-04-19 16:05:55 +0200

    fix: validate password only on registration

--- src/lib/validations/LoginSchema.ts
+++ src/lib/validations/LoginSchema.ts
@@ -2,8 +2,5 @@ import { object, string } from 'zod';
 
 export const LoginSchema = object({
   email: string().email().trim().toLowerCase(),
-  password: string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
-    message:
-      'Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number'
-  })
+  password: string()
 });
--- src/lib/validations/RegistrationSchema.ts
+++ src/lib/validations/RegistrationSchema.ts
@@ -1,5 +1,10 @@
+import { string } from 'zod';
 import { LoginSchema } from './LoginSchema';
 
 export const RegistrationSchema = LoginSchema.extend({
+  password: string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
+    message:
+      'Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number'
+  })
   // TODO: Add additional fields for registration
 });
