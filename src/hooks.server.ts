731eb55e2a988425c633546033a6dd4a84d6b450
Jakub Hrášek <hrasekj@gmail.com>
2024-04-19 16:56:01 +0200

    refactor: move handle logic into hook authValidateSession

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,27 +1,4 @@
-import { setBlankSessionCookie, setSessionCookie, lucia } from '$lib/server/auth';
-import type { Handle } from '@sveltejs/kit';
+import { authValidateSession } from '$lib/server/hooks/authValidateSession';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = async ({ event, resolve }) => {
-  const sessionId = event.cookies.get(lucia.sessionCookieName);
-
-  if (!sessionId) {
-    event.locals.user = null;
-    event.locals.session = null;
-    return resolve(event);
-  }
-
-  const { session, user } = await lucia.validateSession(sessionId);
-
-  if (session?.fresh) {
-    setSessionCookie(event, session);
-  }
-
-  if (!session) {
-    setBlankSessionCookie(event);
-  }
-
-  event.locals.user = user;
-  event.locals.session = session;
-
-  return resolve(event);
-};
+export const handle = sequence(authValidateSession);
---
+++ src/lib/server/hooks/authValidateSession.ts
@@ -0,0 +1,27 @@
+import { lucia, setBlankSessionCookie, setSessionCookie } from '$lib/server/auth';
+import type { Handle } from '@sveltejs/kit';
+
+export const authValidateSession: Handle = async ({ event, resolve }) => {
+  const sessionId = event.cookies.get(lucia.sessionCookieName);
+
+  if (!sessionId) {
+    event.locals.user = null;
+    event.locals.session = null;
+    return resolve(event);
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId);
+
+  if (session?.fresh) {
+    setSessionCookie(event, session);
+  }
+
+  if (!session) {
+    setBlankSessionCookie(event);
+  }
+
+  event.locals.user = user;
+  event.locals.session = session;
+
+  return resolve(event);
+};
