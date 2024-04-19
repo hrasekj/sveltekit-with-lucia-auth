868ffdc658b6415022a292be084d0c9b960ff6cb
Jakub Hrášek <hrasekj@gmail.com>
2024-04-19 15:37:12 +0200

    feat: add Registration schema

--- src/lib/validations/LoginSchema.ts
+++ src/lib/validations/LoginSchema.ts
@@ -1,8 +1,8 @@
-import { z } from 'zod';
+import { object, string } from 'zod';
 
-export const LoginSchema = z.object({
-  email: z.string().email().trim().toLowerCase(),
-  password: z.string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
+export const LoginSchema = object({
+  email: string().email().trim().toLowerCase(),
+  password: string().regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, {
     message:
       'Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number'
   })
